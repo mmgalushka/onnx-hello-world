bee15d4b07b13285f244d7430c8780a212460f0b
Mykola Galushka <mm.galushka@gmail.com>
2021-08-17 23:18:12 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# onnx-hello-world
+# ONNX Hello World
+
+![Project Logo](/logo.png)
+
 The repository contains examples of serializing deserializing different ML models using Open Neural Network Exchange (ONNX).
---
+++ helper.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# =============================================================================
+# HELPER ACTIONS
+# =============================================================================
+
+NC=$(echo "\033[m")
+BOLD=$(echo "\033[1;39m")
+CMD=$(echo "\033[1;34m")
+OPT=$(echo "\033[0;34m")
+
+action_usage(){
+
+                        
+    echo -e "  ___  _   _ _   ___  __"
+    echo -e " / _ \| \ | | \ | \ \/ /"
+    echo -e "| | | |  \| |  \| |\  / "
+    echo -e "| |_| | |\  | |\  |/  \ "
+    echo -e " \___/|_| \_|_| \_/_/\_\\"
+    echo -e "  Hello World           "                   
+    echo -e ""
+    echo -e "This is the project for everyone interested in learning about "
+    echo -e "Open Neural Network Exchange (ONNX).                          "
+    echo -e ""
+    echo -e "ONNX is an open format built to represent machine learning    "
+    echo -e "models. ONNX defines a common set of operators - the building "
+    echo -e "blocks of machine learning and deep learning models - and a   "
+    echo -e "common file format to enable AI developers to use models with "
+    echo -e "a variety of frameworks, tools, runtimes, and compilers.      "
+    echo -e ""
+    echo -e "                                 Quote from: https://onnx.ai/ "
+
+    echo -e "${BOLD}System Commands:${NC}"
+    echo -e "   ${CMD}init${NC} initializers environment;" 
+}
+
+action_init(){
+    if [ -d .venv ];
+        then
+            rm -r .venv
+    fi
+
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip3 install pip --upgrade
+    pip3 install setuptools --upgrade 
+    pip3 install -r requirements.txt
+}
+
+# =============================================================================
+# HELPER COMMANDS SELECTOR
+# =============================================================================
+case $1 in
+    init)
+        action_init
+    ;;
+    *)
+        action_usage
+    ;;
+esac  
+
+exit 0
