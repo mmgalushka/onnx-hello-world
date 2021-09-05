bd85a2c7dc49ae4050b68c2d8e5649915b5d9f6c
Mykola Galushka <mm.galushka@gmail.com>
2021-09-05 15:07:19 +0100

    Added JNB for SKLearn models.

--- README.md
+++ README.md
@@ -2,4 +2,29 @@
 
 ![Project Logo](/logo.png)
 
-The repository contains examples of serializing deserializing different ML models using Open Neural Network Exchange (ONNX).
+[![Project License](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/mmgalushka/bootwrap/blob/main/LICENSE)
+
+his project provides educational material for using [Open Neural Network Exchange (ONNX)](https://onnx.ai/). These materials would be useful for data scientists and engineers who are planning to use the ONNX ML models in their AI projects. Using the prepared notebooks you will be to find answers to the following questions: 
+* How I can convert my ML classification model built using [SKLearn](https://scikit-learn.org/stable/), [XGBoost](https://xgboost.readthedocs.io/en/latest/), and [Tensorflow (Keras)](https://www.tensorflow.org/) into the ONNX format? 
+* What is the difference between the original ML model and the model converted into the ONNX?
+* How I can visualize the ONNX graph?
+* Where I can find and how I can use already trained and serialized ONNX models?
+
+**Note:** In this project, we will not spend time performing data cleaning, feature selection, selecting the best features, etc. This project is about ONNX!
+
+ONNX
+
+Let me cite a quote defining the goal of the ONNX project defined [here](https://onnx.ai/about.html):
+
+> "Our goal is to make it possible for developers to use the right combinations of tools for their project. We want everyone to be able to take AI from research to reality as quickly as possible without artificial friction from toolchains."
+
+If you would like about ONNX please follow the following [link](https://github.com/onnx/).
+
+You might be asking the question: _why do we need this project if we already have excellent documentation [here](https://github.com/onnx/)_? I believe the best way to learn something new is to try-examples-yourself. So I tried to follow the existing documentation, repeat, and introduce some new steps. This way I tried to learn this technology and decided to share my experience with others.
+
+# Links
+
+* ONNX Official Website: https://onnx.ai/
+* ONNX GitHub:https://github.com/onnx
+* Notebooks: https://github.com/mmgalushka/onnx-hello-world
+* Issue Tracker: https://github.com/mmgalushka/onnx-hello-world/issues
---
+++ utils.py
@@ -0,0 +1,140 @@
+import json
+import zipfile
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+
+from skl2onnx.common.data_types import (
+    Int64TensorType,
+    FloatTensorType,
+    DoubleTensorType,
+    StringTensorType
+)
+
+
+class Schema:
+    def __init__(self, descriptor):
+        self.__numeric_features = []
+        self.__categorical_features = []
+        self.__target = None
+
+        for idx, feature in enumerate(descriptor['features']):
+            if idx == descriptor['target']:
+                self.__target = feature['name']
+            else:
+                if 'kind' in feature:
+                    if feature['kind'] == 'numeric':
+                        self.__numeric_features.append(feature['name'])
+                    elif feature['kind'] == 'categorical':
+                        self.__categorical_features.append(feature['name'])
+
+    @property
+    def numeric_features(self):
+        return self.__numeric_features
+
+    @property
+    def categorical_features(self):
+        return self.__categorical_features
+
+    @property
+    def features(self):
+        return self.numeric_features + self.categorical_features
+
+    @property
+    def target(self):
+        return self.__target
+
+
+class Dataset:
+    def __init__(self, schema, data):
+        self.__schema = schema
+        self.__data = data
+
+    def __str__(self):
+        return str(self.__data)
+
+    def __setitem__(self, feature, data):
+        self.__data[feature] = data
+
+    def __getitem__(self, feature):
+        return self.__data[feature]
+
+    def __len__(self):
+        return len(self.__data)
+
+    @property
+    def schema(self):
+        return self.__schema
+
+    @property
+    def X(self):
+        return self.__data[self.__schema.features]
+
+    @property
+    def y(self):
+        return self.__data[self.__schema.target]
+
+    def train_test_split(self):
+        train, test = train_test_split(
+            self.__data, test_size=0.33, random_state=42
+        )
+        return Dataset(self.schema, train), Dataset(self.schema, test)
+
+
+def load_dataset(name):
+    archive = zipfile.ZipFile(f'data/{name}.zip', 'r')
+    with archive.open('descriptor.json') as f:
+        descriptor = json.load(f)
+    with archive.open('data.csv') as f:
+        data = pd.read_csv(f)
+    return Dataset(Schema(descriptor), data)
+
+
+def get_onnx_input_type(dataset, drop=None):
+    input_type = []
+    for k, v in zip(dataset.X.columns, dataset.X.dtypes):
+        if drop is not None and k in drop:
+            continue
+        if v == 'int64':
+            t = Int64TensorType([None, 1])
+        elif v == 'float32':
+            t = FloatTensorType([None, 1])
+        elif v == 'float64':
+            t = DoubleTensorType([None, 1])
+        else:  # v == object
+            t = StringTensorType([None, 1])
+        input_type.append((k, t))
+    return input_type
+
+
+def get_onnx_input_data(dataset):
+    return {
+        column: dataset.X[column].values.reshape(-1, 1)
+        for column in dataset.X.columns
+    }
+
+
+def create_preprocessor(dataset):
+    # The numeric features preprocessing pipeline.
+    numeric_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='median')),
+        ('scaler', StandardScaler())
+    ])
+
+    # The categorical features preprocessing pipeline.
+    categorical_transformer = Pipeline(steps=[
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ])
+
+    # The data preprocessor combines numeric and categorical
+    # column transformers.
+    preprocessor = ColumnTransformer(transformers=[
+        ('num', numeric_transformer, dataset.schema.numeric_features),
+        ('cat', categorical_transformer, dataset.schema.categorical_features)
+    ])
+
+    return preprocessor
